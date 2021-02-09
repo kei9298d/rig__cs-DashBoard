38b237ad58ef5a966227198920e5645bfe966524
Kei Mikage <kei9298d@gmail.com>
2021-02-09 19:03:14 +0900

    change array name.

--- public_html/lib/function_generate_original_dataset.php
+++ public_html/lib/function_generate_original_dataset.php
@@ -12,19 +12,36 @@ function generate_original_dataset($cfg) {
     $cfg['time']['current']['end'] = time();
     $cfg['time']['current']['start'] = $cfg['time']['end'] - ($cfg['hour'] * 60 * 60);
 
+    $cfg['time']['month']['this']['start'] = strtotime('first day of this month');
+    $cfg['time']['month']['this']['start'] = strtotime('last day of this month') + ( 24 * 60 * 60 -1 ); 
+    
     // Load Data set
     $data['csv'] = file($cfg['file']['datafile']);
 
+    // for Current Hour
     foreach ($data['csv'] as $row) {
         $row_array = explode(',', $row);
         // Erase CRLF
         $row_array[1] = str_replace("\n", '', $row_array[1]);
 
         // Filter
-        if( $row_array != '' && $row_array[0] >= $cfg['time']['start'] && $row_array[0] <= $cfg['time']['end'] ) {
+        if( $row_array != '' && $row_array[0] >= $cfg['time']['current']['start'] && $row_array[0] <= $cfg['time']['current']['end'] ) {
             $data['watt']['current'][] = $row_array;
         }
     }
+
+    // for This Month.
+    foreach ($data['csv'] as $row) {
+        $row_array = explode(',', $row);
+        // Erase CRLF
+        $row_array[1] = str_replace("\n", '', $row_array[1]);
+
+        // Filter
+        if( $row_array != '' && $row_array[0] >= $cfg['time']['month']['this']['start'] && $row_array[0] <= $cfg['time']['month']['this']['end'] ) {
+            $data['watt']['month']['this'][] = $row_array;
+        }
+    }
+
     unset($row_array);
     unset($row);
 
