0d544c0a07b63c13b440e53f464bce79885bd296
Kei Mikage <kei9298d@gmail.com>
2021-02-16 15:06:43 +0900

    bug fix.

--- public_html/index.php
+++ public_html/index.php
@@ -33,8 +33,8 @@ require_once ('./lib/function_generate_data_current_month.php');
 require_once ('./lib/function_calc_bill_hour.php');
 require_once ('./lib/function_calc_bill_month.php');
 
-//require_once ('./lib/function_generate_graph_PowerConsumption.php');
-//require_once ('./lib/function_generate_sheet_PowerConsumption.php');
+require_once ('./lib/function_generate_graph_PowerConsumption.php');
+require_once ('./lib/function_generate_sheet_PowerConsumption.php');
 
 //require_once ('./lib/function_generate_graph_PowerBill.php');
 require_once ('./lib/function_generate_sheet_PowerBill.php');
@@ -50,7 +50,7 @@ $output['bill']['month'] = calc_bill_month($cfg, $data['currenet_month']);
 // Generate Graph URL.
 $output['graph']['PowerConsumption']['url'] = generate_graph_PowerConsumption($cfg, $data['currenet_hour']);
 // Generate Sheet Data.
-$output['sheet']['PowerConsumption'] = generate_sheet_PowerConsumption($cfg, $data['currenet_hour']);
+$output['sheet']['PowerConsumption'] = generate_sheet_PowerConsumption($cfg, $data['currenet_month']);
 
 // Generate Graph URL.
 //$data['graph']['PowerBill']['url'] = generate_graph_PowerBill($cfg, $data['currenet_month']);
